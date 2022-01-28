582184722208caf2f480adb87e4ed524ffd20873
tgiannotta <t.giannotta92@gmail.com>
2022-01-28 16:31:47 +0100

    scaffolding di base

--- index.php
+++ index.php
@@ -1,5 +1,40 @@
+<!-- Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online;
+ ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.
+ Strutturare le classi gestendo l'ereditarietà dove necessario; 
+ ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti.
+Provate a stampare in pagina come visto questa mattina i prodotti scelti dall'utente.
+ -->
+
+
+
 <?php
 
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SHOP</title>
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>"> 
+</head>
+<body>
+    <h1>Benvenuto nel negozio di Tommaso</h1>
+    <div class="carrello-container">
+        <div class="carrello">
+                <div class="prodotti">
+                    <h2>Prova prodotti</h2>
+                </div>
+                <div class="totale">
+                    <h2>Prova Totale</h2>
+                </div>
+        </div>
+
+
+    </div>
+</body>
+</html>
